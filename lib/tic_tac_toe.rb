632e7a77ebdaafc3017a358f6ff764d3b4962e9f
Vincent Huang <vincent.huang@wework.com>
2018-02-14 00:37:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,13 @@
 
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],  # Middle row
+  [0,4,8],  # Middle row
+  [2,4,6],
+  [2,5,8],
+  [1,4,7],
+  [0,3,6]
+  # ETC, an array for each win combination
+]
