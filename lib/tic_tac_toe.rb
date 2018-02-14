615e0a034850205cf6f0faafcd2b91b18d7ab851
Vincent Huang <vincent.huang@wework.com>
2018-02-14 00:48:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,6 +90,22 @@ end
 def move(board, index, current_player)
   board[index] = current_player
 end
+def turn_count(board)
+  # board.count{|token| token == "X" || token == "O"}
+
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
+    end
+  end
+  turns
+
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
 
 def play(board)
   while (!over?(board))
